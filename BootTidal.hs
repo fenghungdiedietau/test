a99f942ea8e88bc321a8360b7c545d1e451f2d95
fenghungdiedietau <trnutair@gmail.com>
2018-06-06 15:51:18 +0800

    0606 #3

---
+++ BootTidal.hs
@@ -0,0 +1,22 @@
+:set -XOverloadedStrings
+:set prompt ""
+:module Sound.Tidal.Context
+
+(cps, nudger, getNow) <- cpsUtils'
+
+(d1,t1) <- superDirtSetters getNow
+(d2,t2) <- superDirtSetters getNow
+(d3,t3) <- superDirtSetters getNow
+(d4,t4) <- superDirtSetters getNow
+(d5,t5) <- superDirtSetters getNow
+(d6,t6) <- superDirtSetters getNow
+(d7,t7) <- superDirtSetters getNow
+(d8,t8) <- superDirtSetters getNow
+(d9,t9) <- superDirtSetters getNow
+
+let bsin a b c = begin (slow a ( scale b c $ sine1))
+let bps x = cps (x/2)
+let hush = mapM_ ($ silence) [d1,d2,d3,d4,d5,d6,d7,d8,d9]
+let solo = (>>) hush
+
+:set prompt "tidal> "
