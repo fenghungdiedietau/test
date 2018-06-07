7e878c292c50eb8a572c259f1b3c67f52a40a99e
fenghungdiedietau <trnutair@gmail.com>
2018-06-07 16:04:43 +0800

    boot

--- BootTidal.hs
+++ BootTidal.hs
@@ -14,7 +14,14 @@
 (d8,t8) <- superDirtSetters getNow
 (d9,t9) <- superDirtSetters getNow
 
+let attsin a b c = attack (slow a ( scale b c $ sine1))
+let rsin a b c = release (slow a ( scale b c $ sine1))
 let bsin a b c = begin (slow a ( scale b c $ sine1))
+let attsaw a b c = attack (slow a ( scale b c $ saw1))
+let rsaw a b c = release (slow a ( scale b c $ saw1))
+let bsaw a b c = begin (slow a ( scale b c $ saw1))
+let ssine a b c = speed (slow a ( scale b c $ sine1))
+let ssaw a b c = speed (slow a ( scale b c $ saw1))
 let bps x = cps (x/2)
 let hush = mapM_ ($ silence) [d1,d2,d3,d4,d5,d6,d7,d8,d9]
 let solo = (>>) hush
